abbc66e00d0dccb06030af555326e35c5b106ec8
GubinYuriy <iosdevelop.2022@gmail.com>
2022-12-06 12:27:30 +0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Юрий Губин on 06.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
