5f8729fca9036ef38ca8958f070971fe09cbebe2
GubinYuriy <iosdevelop.2022@gmail.com>
2022-12-06 12:58:49 +0500

    Add VStack for flag's buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = [
+    "UK",
+    "USA",
+    "Bangladesh",
+    "Germany",
+    "Argentina",
+    "Brazil",
+    "Canada",
+    "Greece",
+    "Russia",
+    "Sweden"
+    ].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack {
+                    Text("Choose flag: ")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
